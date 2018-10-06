469d4acc2cfd3ffcad32b1af7a5b50684e99523b
Maplespe <43676879+Maplespe@users.noreply.github.com>
2018-10-06 10:50:26 +0800

    Add files via upload

---
+++ BlackAeroGlass/BlackAeroGlass/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "BlackAeroGlassEX";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.设置ToolStripMenuItem.Text = "设置";
+            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.SettingsToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.EndToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "启用的程序(标题)\r\n一行一个";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(204, 386);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "此电脑\r\n文件资源管理器\r\n命令提示符";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(223, 377);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "启用Win10 API";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(228, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "保存设置";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BlackAeroGlass设置";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BlackAeroGlass/BlackAeroGlass/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 //就这名字吧
+{
+    public partial class Form1 : Form
+    {
+        string text1 = BlackAeroGlass.OperateIniFile.ReadIniData("Text", "value", null, System.IO.Directory.GetCurrentDirectory() + "\\cfg.ini");
+
+        public Form1() { InitializeComponent(); }
+        //取窗口句柄
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+
+        private void SettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //显示窗体
+            Visible = true;
+            WindowState = FormWindowState.Normal;
+            Activate();
+        }
+        private void EndToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //关闭程序
+            notifyIcon1.Visible = false;
+            Close();
+            Dispose();
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //启动循环线程
+            Form1 f = new Form1();
+            Thread nonParameterThread = new Thread(new ThreadStart(f.Cycle));
+            nonParameterThread.Start();
+
+            //读配置
+            textBox1.Text = text1.Replace("&n", "\r\n");
+            if (BlackAeroGlass.OperateIniFile.ReadIniData("win10", "value", null, System.IO.Directory.GetCurrentDirectory() + "\\cfg.ini") == "true")
+            {
+                checkBox1.Checked = true;
+            } else { checkBox1.Checked = false; }
+
+            //隐藏窗体
+            WindowState = FormWindowState.Minimized;
+            notifyIcon1.Visible = true;
+            Visible = false;
+            Hide();
+            this.ShowInTaskbar = false;
+        }
+        #region dwmapi.dll
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MARGINS
+        {
+            public int Left;
+            public int Right;
+            public int Top;
+            public int Bottom;
+        }
+
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        static extern void DwmExtendFrameIntoClientArea(int hwnd, ref MARGINS margins);
+
+        public void LoadAero1(int hwnd)
+        {
+            MARGINS m = new MARGINS();
+            m.Right = -1;
+            DwmExtendFrameIntoClientArea(hwnd, ref m);
+        }
+        #endregion
+        #region win10 能用的AeroAPI user32.dll
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowCompositionAttribute(int hwnd, ref WindowCompositionAttributeData data);
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WindowCompositionAttributeData
+        {
+            public WindowCompositionAttribute Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        internal enum WindowCompositionAttribute
+        {
+            WCA_ACCENT_POLICY = 19
+        }
+
+        internal enum AccentState
+        {
+            ACCENT_DISABLED = 0,
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_INVALID_STATE = 4
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct AccentPolicy
+        {
+            public AccentState AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        public void LoadAero2(int hwnd)
+        {
+            var accent = new AccentPolicy();
+            var accentStructSize = Marshal.SizeOf(accent);
+            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+            data.SizeOfData = accentStructSize;
+            data.Data = accentPtr;
+
+            SetWindowCompositionAttribute(hwnd, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                //隐藏窗口 不要关闭窗口
+                e.Cancel = true;
+                WindowState = FormWindowState.Minimized;
+                notifyIcon1.Visible = true;
+                Hide();
+                return;
+            }
+        }
+
+        private void Cycle()
+        {
+            //循环检测 设定延迟刷新←_←
+            while(true)
+            {
+                Thread.Sleep(1000);
+                text1 = text1.Replace("&n", "\r\n");
+                string[] s = text1.Split(new char[] { '\n' });
+                for (int i = 0; i < s.Length; i++)
+                {
+                    //替换掉字符 换行 回车 行文本拆开
+                    s[i] = s[i].Replace("\r", "");
+                    Debug.WriteLine(s[i]);
+                    int IntPtrs= FindWindow(null, s[i]);
+                    Debug.WriteLine(IntPtrs );
+                    if (checkBox1.Checked)
+                    {
+                        LoadAero2(IntPtrs);
+                    } else { LoadAero1(IntPtrs); }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //保存配置项
+            string Text = textBox1.Text;
+            bool returns = BlackAeroGlass.OperateIniFile.WriteIniData("Text", "value", Text.Replace("\r\n", "&n"), System.IO.Directory.GetCurrentDirectory() + "\\cfg.ini");
+            if (checkBox1.Checked)
+            {
+              returns =  BlackAeroGlass.OperateIniFile.WriteIniData ("win10","value","true", System.IO.Directory.GetCurrentDirectory() +"\\cfg.ini");
+            } else { returns = BlackAeroGlass.OperateIniFile.WriteIniData ("win10", "value", "false", System.IO.Directory.GetCurrentDirectory() + "\\cfg.ini");}
+            if (returns)
+            {
+                MessageBox.Show("保存配置成功","提示",MessageBoxButtons.OK ,MessageBoxIcon.Asterisk );
+            } else { MessageBox.Show("保存配置失败..", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error );  }
+        }
+    }
+}
